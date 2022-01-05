4efdff596dd27da982d8c1feda5a421729ce2e55
olivier3 <olivierbilodeau@outlook.com>
2022-01-05 16:14:57 -0500

    project name update and basic mouvement

---
+++ DDGoS/Assets/Scripts/CharacterMouvement.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class CharacterMouvement : MonoBehaviour
+{
+    private float JumpForce = 3f;
+    public float MoveSpeed = 2f;
+    private GameObject ObjectCharacter;
+    private Rigidbody2D ObjectRigidbody;
+    public bool isGrounded = true;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        ObjectRigidbody = GetComponent<Rigidbody2D>();
+        ObjectCharacter = GameObject.Find("Character");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+            transform.position += Time.deltaTime * MoveSpeed * Vector3.right;
+
+        if (Input.GetKey(KeyCode.A))
+            transform.position += Time.deltaTime * MoveSpeed * Vector3.left;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
+            ObjectRigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Square")
+        {
+            Debug.Log("Trigger Enter");
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Square")
+        {
+            Debug.Log("Trigger Exit");
+            isGrounded = false;
+        }
+    }
+}
