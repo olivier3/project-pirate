67fc54575e8d273496e0c51d0b51c78801c20c00
olivier3 <olivierbilodeau@outlook.com>
2022-01-18 16:11:13 -0500

    Fix bomb attack and add bomb explosion script

--- DDGoS/Assets/Scripts/AttackBomb.cs
+++ DDGoS/Assets/Scripts/AttackBomb.cs
@@ -22,10 +22,45 @@ public class AttackBomb : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             GameObject bombIgnited = Instantiate(Bomb, transform.position, transform.rotation);
+
+            var position = bombIgnited.transform.position;
+            position.y += 0.3f;
+
             if (ObjectCharacterImage.GetComponent<SpriteRenderer>().flipX)
-                bombIgnited.GetComponent<Rigidbody2D>().AddForce(new Vector2(-2f, 5f), ForceMode2D.Impulse);
+            {
+                position.x -= 0.3f;
+                bombIgnited.GetComponent<Rigidbody2D>().AddForce(new Vector2(-3f, 4f), ForceMode2D.Impulse);
+            }
             else
-                bombIgnited.GetComponent<Rigidbody2D>().AddForce(new Vector2(2f, 5f), ForceMode2D.Impulse);
+            {
+                position.x += 0.3f;
+                bombIgnited.GetComponent<Rigidbody2D>().AddForce(new Vector2(3f, 4f), ForceMode2D.Impulse);
+            }
+
+            bombIgnited.GetComponent<BombExplosion>().Ignited = true;
+
+            bombIgnited.transform.position = position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            GameObject bombIgnited = Instantiate(Bomb, transform.position, transform.rotation);
+
+            var position = bombIgnited.transform.position;
+
+            if (ObjectCharacterImage.GetComponent<SpriteRenderer>().flipX)
+            {
+                position.x += 0.4f;
+            }
+            else
+            {
+                position.x -= 0.4f;
+            }
+
+            bombIgnited.GetComponent<BombExplosion>().Ignited = true;
+
+            bombIgnited.transform.position = position;
         }
     }
 }
+
